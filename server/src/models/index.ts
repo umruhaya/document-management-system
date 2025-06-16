dfd9fd0e232e6f7dca20bd7294f44de57774d029
Umer Naeem <umernaeem135acc@gmail.com>
2025-06-16 22:13:56 +0500

    feat: :sparkles: consolidate db tables and export them under a unfied interface for orm use

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,1 +1,9 @@
-export const table = {}
+import { documentAccess } from './document-access'
+import { documents } from './documents'
+import { users } from './users'
+
+export const table = {
+	users,
+	documents,
+	documentAccess,
+}
