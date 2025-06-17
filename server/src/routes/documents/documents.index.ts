8c34e01dd8c5b153acbfcf6235d23356dba51cb4
Umer Naeem <umernaeem135acc@gmail.com>
2025-06-17 18:45:54 +0500

    minor: register create doc endpoint

--- server/src/routes/documents/documents.index.ts
+++ server/src/routes/documents/documents.index.ts
@@ -5,10 +5,12 @@ import { getDocumentAccessList } from './documents.access.get'
 import { patchDocumentAccess } from './documents.access.patch'
 import { createDocumentLink } from './documents.link.post'
 import { downloadDocumentByLink } from './documents.download.get'
+import { createDocument } from './documents.post'
 
 export const documentsRouter = createRouter()
 	.openapi(...searchDocuments)
 	.openapi(...getDocumentById)
+	.openapi(...createDocument)
 	.openapi(...getDocumentAccessList)
 	.openapi(...patchDocumentAccess)
 	.openapi(...createDocumentLink)
