f3251c5fbf5ba3082f078147cb2f046c33423d9d
Umer Naeem <umernaeem135acc@gmail.com>
2025-06-17 17:12:58 +0500

    feat: :sparkles: add document endpoint that implements search functionality by metadata filters

--- server/src/routes/documents/documents.get.ts
+++ server/src/routes/documents/documents.get.ts
@@ -4,23 +4,100 @@ import * as HttpStatusCodes from 'stoker/http-status-codes'
 import * as HttpStatusPhrases from 'stoker/http-status-phrases'
 import { jsonContent } from 'stoker/openapi/helpers'
 import type { AppRouteHandler } from '~/lib/types'
+import { db, table } from '~/db'
+import { jwtMiddleware } from '~/middlewares/jwt'
+import { and, arrayContains, eq, exists, ilike, inArray, like, sql } from 'drizzle-orm'
 
 const route = createRoute({
 	method: 'get',
 	path: '/documents',
-	operationId: 'getDocument',
+	operationId: 'getDocuments',
 	tags: ['Documents'],
-	summary: 'Retrieve Details of a Document if it exists',
+	summary: 'Retrieve Documents List By Search Filters',
+	middleware: [jwtMiddleware()],
+	security: [{ jwt: [] }],
+	request: {
+		query: z.object({
+			title: z.string().optional(),
+			author: z.string().optional(),
+			tags: z.string().array().optional(),
+			fileType: z.string().optional(),
+			version: z.coerce.number().optional(),
+			limit: z.number().int().positive().max(50),
+			offset: z.number().int().nonnegative(),
+			exlcudeContent: z.boolean().default(true).optional(),
+		}),
+	},
 	responses: {
 		[HttpStatusCodes.OK]: jsonContent(
-			z.object({}),
+			z.object({
+				documents: z.array(z.object({
+					id: z.string(),
+					title: z.string(),
+					description: z.string(),
+					fileType: z.string(),
+					version: z.number(),
+					size: z.number(),
+					content: z.string(),
+					tags: z.array(z.string()),
+					createdAt: z.string(),
+					updatedAt: z.string(),
+				})),
+			}),
 			HttpStatusPhrases.OK,
 		),
 	},
 })
 
 const handler: AppRouteHandler<typeof route> = async (c) => {
-	return c.json({}, HttpStatusCodes.OK)
+	const { userId } = c.get('jwtPayload')
+	const query = c.req.valid('query')
+
+	const documents = await db.selectDistinctOn([table.documents.id], {
+		id: table.documents.id,
+		title: table.documents.title,
+		description: table.documents.description,
+		fileType: table.documents.fileType,
+		version: table.documents.version,
+		size: table.documents.size,
+		content: query.exlcudeContent ? table.documents.content : sql`SELECT ""`,
+		tags: table.documents.tags,
+		createdAt: table.documents.createdAt,
+		updatedAt: table.documents.updatedAt,
+	})
+		.from(table.documents)
+		.innerJoin(table.documentAccess, eq(table.documents.id, table.documentAccess.documentId))
+		.where(
+			and(
+				// First we need to check that the user querying should have access to the document in some capacity
+				exists(
+					db.select()
+						.from(table.documentAccess)
+						.where(
+							and(
+								eq(table.documentAccess.documentId, table.documents.id),
+								eq(table.documentAccess.userId, userId),
+							),
+						),
+				),
+				// case insensitive match for title
+				query.title ? ilike(table.documents.title, `%${query.title}%`) : undefined,
+				// author (user is said to be author if they have owner or editor access)
+				// Ensure querying user has access (via EXISTS)
+				query.author ? ilike(table.users.username, `%${query.author}%`) : undefined,
+				query.author ? inArray(table.documentAccess.role, ['owner', 'editor']) : undefined,
+				// tags
+				query.tags && query.tags.length !== 0 ? arrayContains(table.documents.tags, query.tags) : undefined,
+				// file
+				query.fileType ? eq(table.documents.fileType, query.fileType) : undefined,
+			),
+		)
+		.limit(query.limit)
+		.offset(query.offset)
+		.execute()
+		.then(r => r)
+
+	return c.json({ documents }, HttpStatusCodes.OK)
 }
 
-export const get = [route, handler] as const
+export const searchDocuments = [route, handler] as const
